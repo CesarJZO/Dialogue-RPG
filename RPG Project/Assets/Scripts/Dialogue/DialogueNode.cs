5ca631c60308530f9d8f7367109c6d5a9f7e9dd9
César Zárate <cesarjzo@outlook.com>
2023-05-16 18:25:45 -0600

    Add Dialogue.cs SO and DialogueNode.cs class

---
+++ RPG Project/Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dialogue
+{
+    [Serializable]
+    public class DialogueNode
+    {
+        public string id;
+
+        [TextArea] public string text;
+
+        public string children;
+    }
+}
