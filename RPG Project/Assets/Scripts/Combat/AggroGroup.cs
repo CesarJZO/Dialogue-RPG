07b4a33ce13f292bf48edd732e0f2d7a283da11f
CesarJZO <cesarjzo@outlook.com>
2023-06-15 17:16:04 -0600

    Add AggroGroup.cs

---
+++ RPG Project/Assets/Scripts/Combat/AggroGroup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class AggroGroup : MonoBehaviour
+    {
+        [SerializeField] private Fighter[] fighters;
+        [SerializeField] private bool activateOnStart;
+
+        private void Start()
+        {
+            Activate(activateOnStart);
+        }
+
+        public void Activate(bool activate)
+        {
+            foreach (Fighter fighter in fighters)
+            {
+                if (fighter.TryGetComponent(out CombatTarget combatTarget))
+                    combatTarget.enabled = activate;
+                fighter.enabled = activate;
+            }
+        }
+    }
+}
