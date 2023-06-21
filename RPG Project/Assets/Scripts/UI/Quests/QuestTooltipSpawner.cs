1e5d4a59fbb991939b488130510cc05a22ab87d4
CesarJZO <cesarjzo@outlook.com>
2023-06-21 17:20:43 -0600

    Add Tooltip spawner and Toggle method

---
+++ RPG Project/Assets/Scripts/UI/Quests/QuestTooltipSpawner.cs
@@ -0,0 +1,18 @@
+﻿using GameDevTV.Core.UI.Tooltips;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestTooltipSpawner : TooltipSpawner
+    {
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+
+        }
+
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+    }
+}
