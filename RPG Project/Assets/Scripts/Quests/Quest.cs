feedc0a4da77db3116c0ed08aed5b7be69cb3084
CesarJZO <cesarjzo@outlook.com>
2023-06-21 17:51:36 -0600

    Add test Quests SOs and put them in UI

---
+++ RPG Project/Assets/Scripts/Quests/Quest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RPG.Quests
+{
+    [CreateAssetMenu(fileName = "New Quest", menuName = "Quest", order = 0)]
+    public class Quest : ScriptableObject
+    {
+        [SerializeField] private string[] objectives;
+
+        public string Title => name;
+        public int ObjectiveCount => objectives.Length;
+    }
+}
---
+++ RPG Project/Assets/Scripts/UI/Quests/QuestListUI.cs
@@ -0,0 +1,25 @@
+﻿using RPG.Quests;
+using UnityEngine;
+
+namespace RPG.UI.Quests
+{
+    public class QuestListUI : MonoBehaviour
+    {
+        [SerializeField] private Quest[] quests;
+        [SerializeField] private QuestItemUI questPrefab;
+
+        private void Start()
+        {
+            foreach (Transform t in transform)
+            {
+                Destroy(t.gameObject);
+            }
+
+            foreach (Quest quest in quests)
+            {
+                QuestItemUI questInstance = Instantiate(questPrefab, transform);
+                questInstance.Setup(quest);
+            }
+        }
+    }
+}
