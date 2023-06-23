ac256b459c0b6ccefea575f4ebdea2e494052c05
CesarJZO <cesarjzo@outlook.com>
2023-06-23 16:51:08 -0600

    Add complex conditions using disjunction, conjunction and negation

--- RPG Project/Assets/Scripts/Core/Condition.cs
+++ RPG Project/Assets/Scripts/Core/Condition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace RPG.Core
@@ -7,19 +8,42 @@ namespace RPG.Core
     [Serializable]
     public class Condition
     {
-        [SerializeField] private string predicate;
-        [SerializeField] private string[] parameters;
+        [SerializeField] private Disjunction[] and;
+
+        [Serializable]
+        public class Disjunction
+        {
+            [SerializeField] private Predicate[] or;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                return or.Any(predicate => predicate.Check(evaluators));
+            }
+        }
+
+        [Serializable]
+        public class Predicate
+        {
+            [SerializeField] private string predicate;
+            [SerializeField] private string[] parameters;
+            [SerializeField] private bool negate;
+
+            public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
+            {
+                foreach (IPredicateEvaluator evaluator in evaluators)
+                {
+                    bool? result = evaluator.Evaluate(predicate, parameters);
+                    if (result is null) continue;
+                    if (result == negate) return false;
+                }
+
+                return true;
+            }
+        }
 
         public bool Check(IEnumerable<IPredicateEvaluator> evaluators)
         {
-            foreach (IPredicateEvaluator evaluator in evaluators)
-            {
-                bool? result = evaluator.Evaluate(predicate, parameters);
-                if (result is null) continue;
-                if (result == false) return false;
-            }
-
-            return true;
+            return and.All(disjunction => disjunction.Check(evaluators));
         }
     }
 }
